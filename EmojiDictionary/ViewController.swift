172b369a926c08942d2c4bdea5c51e31643e2e9b
Peter Pickett <peter@pickettbrass.com>
2017-08-20 13:33:09 -0400

    Got second view in

--- EmojiDictionary/ViewController.swift
+++ EmojiDictionary/ViewController.swift
@@ -8,13 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var thecooltableview: UITableView!
+    
+    var emojis = ["😀", "😎", "😣", "😭","👏","blah blah blah blah blah blah balh"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        thecooltableview.dataSource = self
+        thecooltableview.delegate = self
     }
 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text = emojis[indexPath.row]
+        
+        return cell
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
